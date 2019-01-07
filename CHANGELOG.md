5e3b965760d9b3ff43c2127c2ada20ae7ff0938a
LupusVII <jln.thirion@gmail.com>
2019-01-07 11:57:18 +0100

    + Add dcaWizard as dependancy + Add current time as "createdDate" field + Add categories to locations * Update Readme and Changelog files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 Extension "Locations" for Contao Open Source CMS
 ========
 
+0.5.0 - 2018-01-07
+- Add tl_content as child table of tl_wem_location, so we can have a subpage for each item
+
+0.4.0 - 2018-01-04
+- Rework the way maps, options and markers are handle
+
 0.3.0 - 2018-09-24
 - Add Leaflet library, thanks to Ollie
 
--- config/config.php
+++ config/config.php
@@ -18,7 +18,7 @@ array_insert($GLOBALS['BE_MOD'], 1, array
 	(
 		'wem-maps' => array
 		(
-			'tables'      => array('tl_wem_map', 'tl_wem_location', 'tl_content'),
+			'tables'      => array('tl_wem_map', 'tl_wem_map_category', 'tl_wem_location', 'tl_content'),
 			'import'	  => array('WEM\Location\Backend\Callback', "importLocations"),
 			'export'	  => array('WEM\Location\Backend\Callback', "exportLocations"),
 			'geocode'	  => array('WEM\Location\Backend\Callback', "geocode"),
