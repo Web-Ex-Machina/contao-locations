7fc230aeccc19c3290c8d9deae166a1f62aabb3b
LupusVII <jln.thirion@gmail.com>
2019-01-09 00:28:39 +0100

    + Add markers updates to Google Maps

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -1,19 +1,24 @@
-// https://developers.google.com/maps/documentation/javascript/reference/3.exp/map
 $(function(){
 	objMapData.forEach(function(location,index){
 		objMarkers[location.id].latLng = new google.maps.LatLng(parseFloat(location.lat), parseFloat(location.lng));
 	});
 
 	objMap = new google.maps.Map($map[0]);
-
 	objMapBounds = new google.maps.LatLngBounds();
+	var options = {};
+
 	for(var i in objMarkers){
 		objMapBounds.extend(objMarkers[i].latLng);
-		objMarkers[i].marker = new google.maps.Marker({
-			position: objMarkers[i].latLng,
-			map: objMap,
-			title: objMarkers[i].name
-		});
+		options = {};
+
+		options.position = objMarkers[i].latLng;
+		options.map = objMap;
+		options.title = objMarkers[i].title;
+
+		if(objMarkers[i].category.marker && objMarkers[i].category.marker.icon)
+			options.icon = objMarkers[i].category.marker.icon.iconUrl;
+
+		objMarkers[i].marker = new google.maps.Marker(options);
 
 		objMarkers[i].marker.addListener('click', function() {
 			alert("CLIC");
